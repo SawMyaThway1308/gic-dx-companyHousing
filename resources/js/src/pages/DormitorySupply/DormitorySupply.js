0f9fc1a44c992012cace9b5e36833ce5431813a9
cherry soe <cherrysoe@gicjp.com>
2023-09-12 17:14:40 +0630

    【作成】備品新規

--- resources/js/src/pages/DormitorySupply/DormitorySupply.js
+++ resources/js/src/pages/DormitorySupply/DormitorySupply.js
@@ -4,66 +4,74 @@ import dummyData from "../../../src/constants/dummyData"
 import './DormitorySupply.scss'
 import CommonStrings from "../../locales/Common";
 
-
 function DormitorySupply() {
-    const getDormitoryName = (name) => {
+    const getDormitoryName = (label, name) => {
         switch(name) {
-            case 2: return "dormitory-supply-label-name-2"
-            case 3: return "dormitory-supply-label-name-3"
-            case 4: return "dormitory-supply-label-name-4"
-            case 5: return "dormitory-supply-label-name-5"
-            default: return "dormitory-supply-label-name"
+            case 2: return `${label}-2`
+            case 3: return `${label}-3`
+            case 4: return `${label}-4`
+            case 5: return `${label}-5`
+            default: return `${label}`
         }
     }
 
     return (
-        // <Box className="dormitory-supply">
-        //     <HStack className="dormitory-list">
-        //         <Box className="dormitory-list-label">
-        //             {dummyData?.equipment?.map((item, index) => (
-        //                 <HStack className={`equipment-list-${index + 1}`}>
-        //                     <Text className={`equipment-list-name`}>{item.name}</Text>
-        //                     <Text className="equipment-require">{equipment.required}</Text>
-        //                 </HStack>
-        //             ))}
-        //         </Box>
-        //         <Box className="dormitory-list-input-form">
-        //             <FormControl>
-        //                 <Select placeholder='Select country'>
-        //                     <option>United Arab Emirates</option>
-        //                     <option>Nigeria</option>
-        //                 </Select>
-        //                 <Input />
-        //             </FormControl>
-        //         </Box>
-        //     </HStack>
-        //     <Divider className="divider" />
-        // </Box>
         <Box className="dormitory-supply">
                 <Box className="dormitory-supply-label">
                 {dummyData?.equipment?.map((item, index) => (
-                    <Box key={index}>
+                    <>
                         <HStack className={`dormitory-supply-label-${index + 1}`}>
-                            <Text className="dormitory-supply-name">
-                                <span className={getDormitoryName(item.name)}>{item.name}</span>
+                            <Text className="dormitory-supply-label-name">
+                                <span className={getDormitoryName("dormitory-supply-label-name", item.name.length)}>{item.name}</span>
                             </Text>
                             <Text className="dormitory-supply-label-require">
-                                <span>{CommonStrings.equipment.required}</span>
+                                <span className="dormitory-supply-label-require-name">{CommonStrings.equipment.required}</span>
                             </Text>
-                        </HStack>
-                        {index < dummyData?.equipment?.length -1 &&
-                            <Divider className="dormitory-supply-label-divider" w={1032} h={0.3}/>
+                            {index < dummyData?.equipment?.length -1 &&
+                            <Divider className="dormitory-supply-label-divider"/>
                         }
-                    </Box>
+                        </HStack>
+                    </>
                 ))}
                 </Box>
                 <Box className="dormitory-supply-data">
-                    <FormControl>
-                        <Select placeholder='Select country'>
-                            <option>United Arab Emirates</option>
-                            <option>Nigeria</option>
-                        </Select>
-                        {/* <Input /> */}
+                    <FormControl mt={0}>
+                        <Box className="equipment-type">
+                            <Select placeholder='未設定'>
+                                <option>United Arab Emirates</option>
+                                <option>Nigeria</option>
+                            </Select>
+                        </Box>
+                        <Box className="equipment-code">
+                            <Input />
+                        </Box>
+                        <Box className="equipment-name">
+                            <Input />
+                        </Box>
+                        <HStack>
+                            {/* <CalendarIcon /> */}
+                            <Box className="equipment-purchase-option">
+                                <Select placeholder='未設定'>
+                                    <option>United Arab Emirates</option>
+                                    <option>Nigeria</option>
+                                </Select>
+                            </Box>
+                            <Text className="equipment-purchase-year">
+                                <span className="name">年</span>
+                            </Text>
+                        </HStack>
+                        <Box className="equipment-maker">
+                            <Input />
+                        </Box>
+                        <Box className="equipment-model">
+                            <Input />
+                        </Box>
+                        <Box className="equipment-address">
+                            <Select placeholder='未設定'>
+                                <option>United Arab Emirates</option>
+                                <option>Nigeria</option>
+                            </Select>
+                        </Box>
                     </FormControl>
                 </Box>
         </Box>
